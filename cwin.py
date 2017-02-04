8943d1f7270c20a26c26ceadd13a51505e90a2d8
Folae Folc <Loodoor@users.noreply.github.com>
2017-02-04 18:54:56 +0100

    Update cwin.py

--- cwin.py
+++ cwin.py
@@ -5,27 +5,41 @@ import sys
 import socket
 
 
-if sys.argv[1]:
-        if os.path.exists("/mnt/c/" + sys.argv[1]):
-                os.chdir("/mnt/c/" + sys.argv[1])
+# intercepting command line arguments
+if sys.argv[1:]:
+        if sys.argv[1] != '-h':
+                if os.path.exists("/mnt/c/" + sys.argv[1]):
+                        os.chdir("/mnt/c/" + sys.argv[1])
+                else:
+                        print("Path does not exist. Setting path to /mnt/c")
+                        os.chdir("/mnt/c/")
         else:
-                print("Path does not exist")
-                exit(1)
+                print("""Help message for ShLoWin
+Version 0.0.1
+usage : cwin.py [path]
+ - path should be a valid path to a Windows folder, refering to C:\\
+        ex: cwin.py Users => start a shell in C:\\Users
+
+""")
 else:
         os.chdir("/mnt/c")
-        exit(1)
 
-HOST = ''
+# some constants
+HOST = ''  # on localhost
 PORT = 50007
 ADDR = HOST, PORT
 BUFFER = 2048
 BIGBUFFER = 8192
+PS1 = "$"
+
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True:
-        c, *args = input("C:/" + os.getcwd()[7:] + "$ ").split(" ")
+        c, *args = input(os.getcwd()[5:6] + ":/" + os.getcwd()[7:] + PS1 + " ").split(" ")
         if c == "q":
                 break
+        elif c == "ps1":
+                PS1 = *args[0:1] if args[0:1] else PS1
         elif c == "cd":
                 os.chdir(os.getcwd() + "/" + "".join(args))
         elif c == "w":
